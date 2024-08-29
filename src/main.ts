fa0b908f52ead9e8a1df05b2da65f38a5d3c4f24
mhammadalam <mhammadalam112@gmail.com>
2024-08-30 02:20:16 +0500

    AWS changes

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //app.useGlobalPipes(new ValidationPipe({ whitelist: true , transform: true }));
   app.useGlobalFilters(new GlobalExceptionFilter());
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
