20a51b0e7dc12c6260d2ad4e61ce1dc9fd077088
mhammadalam <mhammadalam112@gmail.com>
2024-09-02 03:57:31 +0500

    AWS changes final

--- src/common/middleware/file-upload.middleware.ts
+++ src/common/middleware/file-upload.middleware.ts
@@ -1,29 +1,44 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import * as multer from 'multer';
+import * as multerS3 from 'multer-s3';
+import * as AWS from 'aws-sdk';
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
+
+var fileName;
 
 @Injectable()
 export class FileUploadMiddleware implements NestMiddleware {
   private upload = multer({
-    storage: multer.diskStorage({
-      destination: './uploads',
-      filename: (req, file, cb) => {
-        console.log("inside multer");
+    storage: multerS3({
+      s3: s3,
+      bucket: process.env.S3_BUCKET_NAME,
+      metadata: (req, file, cb) => {
+        cb(null, { fieldName: file.fieldname });
+      },
+      key: (req, file, cb) => {
         const uniqueSuffix = Date.now();
-        cb(null, uniqueSuffix + '-' + file.originalname);
+        fileName = uniqueSuffix + '-' + file.originalname;
+        cb(null, fileName);
       },
     }),
   }).single('image');
 
   use(req: Request, res: Response, next: NextFunction) {
-    console.log("inside middlewareee");
     this.upload(req, res, (err) => {
+      console.log("req.file: " + JSON.stringify(req.file));
       if (err) {
         return next(err);
       }
       if (req.file) {
-        
-        req.body.image = req.file.filename;
+        req.body.image = fileName;
       }
       next();
     });
